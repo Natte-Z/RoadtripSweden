29757f5da484c5db9dcfabee89e32496c0ec1523
Natte-Z <nathalie.zauels@gmail.com>
2020-07-29 18:06:12 +0000

    Added second map on Stockholm page, changed layout of contactform on contact page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,23 +1,22 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 7,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 63.8333644,
+            lng: 20.2233067
             }
         });
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-        var locations = [{
-            lat: 40.785091,
-            lng: -73.968285
-        }, {
-            lat: 41.084045,
-            lng: -73.874256
-        }, {
-            lat: 40.754932,
-            lng: -73.984016
-        }];
+        var locations = [
+            { lat: 63.8333644, lng: 20.2233067 },
+            { lat: 67.8531, lng: 20.2255 },
+            { lat: 68.349541, lng: 18.831241 },
+            { lat: 68.3591, lng: 18.7753 },
+            { lat: 66.5901, lng: 19.9012 },
+            { lat: 68.261817, lng: 17.253792 },
+            { lat: 68.1960, lng: 14.395999 },
+        ];
 
         var markers = locations.map(function(location, i) {
             return new google.maps.Marker({
--- assets/js/mapsStockholm.js
+++ assets/js/mapsStockholm.js
@@ -2,22 +2,18 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 59.334591,
+            lng: 18.063240
             }
         });
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-        var locations = [{
-            lat: 40.785091,
-            lng: -73.968285
-        }, {
-            lat: 41.084045,
-            lng: -73.874256
-        }, {
-            lat: 40.754932,
-            lng: -73.984016
-        }];
+        var locations = [
+            { lat: 59.334591, lng: 18.063240 },
+            { lat: 59.321743, lng: 17.886305 },
+            { lat: 59.6191463, lng: 17.7234191 },
+            { lat: 60.012778, lng: 20.039167 },
+        ];
 
         var markers = locations.map(function(location, i) {
             return new google.maps.Marker({
